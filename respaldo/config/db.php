3397eaa29352d2aef78e3f8d90b71606a709d96d
adminproconty <christian.teran@proconty.com>
2018-05-30 16:48:28 -0500

    eliminarespaldo

--- respaldo/config/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*Datos de conexion a la base de datos*/
-define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
-define('DB_USER', 'proco389_usersgb');//Usuario de tu base de datos
-define('DB_PASS', 'barlovento2018');//Contraseña del usuario de la base de datos
-define('DB_NAME', 'proco389_sgbbarlovento');//Nombre de la base de datos
-?>
